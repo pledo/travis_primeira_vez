01f2478beae3e742b6bf493be0b0291e6193b846
Paulo Ledo <paulofledo@gmail.com>
2015-10-21 19:02:13 -0200

    Testando o travis

---
+++ principal.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def somar(x, y):
+    """docstring for soamr"""
+    return(x + y)
---
+++ test.py
@@ -0,0 +1,7 @@
+import pytest
+from principal import somar
+
+def test_somar():
+    """docstring for test_somar"""
+    assert somar(2,4) == 6
+
